6eb93cdf98386f9bae56f4c876b72f34721267fb
GunwooJo <kanggi1997@naver.com>
2024-05-28 14:46:45 +0900

    프로젝트 초기셋팅

---
+++ src/test/java/kangnamuniv/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package kangnamuniv.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
