3b0b7fc6e9b4e7b90b6327f09d1e76505144ca72
GunwooJo <kanggi1997@naver.com>
2024-05-29 01:20:18 +0900

    feat: 로그인 실패할 경우 경고문구 추가

--- src/main/java/kangnamuniv/elibrary/controller/UserController.java
+++ src/main/java/kangnamuniv/elibrary/controller/UserController.java
@@ -6,8 +6,11 @@ import kangnamuniv.elibrary.entity.User;
 import kangnamuniv.elibrary.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -21,7 +24,7 @@ public class UserController {
     public String signUp(@ModelAttribute UserDTO userDTO) {
 
         userService.signUp(userDTO);
-        return "redirect:/user/login.html";
+        return "redirect:/user/login";
     }
 
     @PostMapping("/user/login")
@@ -30,9 +33,19 @@ public class UserController {
         Optional<User> loggedInUser = userService.login(userDTO);
 
         if (loggedInUser.isEmpty()) {
-            return "redirect:/user/login";
+            return "redirect:/user/login?error=true";
         }
         session.setAttribute("loggedInUser", loggedInUser.get());
         return "redirect:/home";
+    }
+
+    @GetMapping("/user/login")
+    public String loginPage(@RequestParam(required = false) String error, Model model) {
+
+        if ("true".equals(error)) {
+            model.addAttribute("loginErrorMsg", "존재하지 않는 회원이거나 비밀번호가 일치하지 않습니다.");
+        }
+
+        return "/user/login";
     }
 }
