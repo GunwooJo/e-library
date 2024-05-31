78990644a2b088009593ab3206beffa168930311
GunwooJo <kanggi1997@naver.com>
2024-05-31 15:26:35 +0900

    feat: title로 책 검색하는 dao 코드, 인철님 코드 merge.

---
+++ src/main/java/kangnamuniv/elibrary/repository/BookDAO.java
@@ -0,0 +1,12 @@
+package kangnamuniv.elibrary.repository;
+
+import kangnamuniv.elibrary.entity.Book;
+
+import java.util.List;
+
+public interface BookDAO {
+    List<Book> getBook();
+    void saveBook(Book book);
+    Book findById(int id);
+    List<Book> searchBooksByTitle(String searchWord);
+}
---
+++ src/main/java/kangnamuniv/elibrary/repository/BookMemoryDAOImpl.java
@@ -0,0 +1,47 @@
+package kangnamuniv.elibrary.repository;
+
+import kangnamuniv.elibrary.entity.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class BookMemoryDAOImpl implements BookDAO{
+    private List<Book> database;
+
+    public BookMemoryDAOImpl() {
+        database = new ArrayList<>();
+        database.add(new Book(1,"제목1","저자1","출판사1","PDF경로1",3));
+        database.add(new Book(2,"제목2","저자2","출판사2","PDF경로2",3));
+        database.add(new Book(3,"제목3","저자3","출판사3","PDF경로3",3));
+    }
+
+    @Override
+    public List<Book> getBook() {
+        return database;
+    }
+
+    @Override
+    public void saveBook(Book book) {
+        database.add(book);
+    }
+
+    @Override
+    public Book findById(int id){
+        for (Book book : database) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Book> searchBooksByTitle(String searchWord) {
+        return database.stream()
+                .filter(book -> book.getTitle().contains(searchWord))
+                .collect(Collectors.toList());
+    }
+}
