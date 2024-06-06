a0a7aca2663d2c9ac807c7c3c4ffb32c046184ff
ny030303 <51257615+ny030303@users.noreply.github.com>
2024-06-06 19:00:44 +0900

    pdf 이름이 한글+특수문자 포함시 열람이 불가한 상태(미수정 중간저장)

--- src/main/java/kangnamuniv/elibrary/controller/FileApiController.java
+++ src/main/java/kangnamuniv/elibrary/controller/FileApiController.java
@@ -8,34 +8,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 
 @RequestMapping("/pdf")
 @RestController
 public class FileApiController {
 
-    private static final String BASE_DIR = System.getProperty("user.dir") + "\\src\\main\\resources\\uploads\\pdf\\";
+    private static final String BASE_DIR = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "uploads" + File.separator + "pdf" + File.separator;
     private static final String DEFAULT_FILE = "Document_Notice_English.pdf";
 
     @GetMapping("/{name}")
     public ResponseEntity<InputStreamResource> getTermsConditions(@PathVariable("name") String name) throws FileNotFoundException {
-        File file = new File(BASE_DIR + name);
+        // Decode the URL-encoded file name
+        String decodedName = UriUtils.decode(name, StandardCharsets.UTF_8);
+
+        // 디버깅을 위해 파일 경로와 파일 이름을 출력합니다.
+        System.out.println("Decoded file name: " + decodedName);
+        System.out.println("File path: " + BASE_DIR + decodedName);
+
+        // 파일 경로에서 인코딩된 파일 이름을 사용
+        String encodedFilePath = BASE_DIR + decodedName;
+        File file = new File(encodedFilePath);
         if (!file.exists() || !file.isFile()) {
+            System.out.println("File not found. Using default file.");
             file = new File(BASE_DIR + DEFAULT_FILE);
         }
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("content-disposition", "inline;filename=" + file.getName());
+        String encodedFileName = UriUtils.encode(file.getName(), StandardCharsets.UTF_8);
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename*=UTF-8''" + encodedFileName);
         InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
         return ResponseEntity.ok()
                 .headers(headers)
                 .contentLength(file.length())
-                .contentType(MediaType.parseMediaType("application/pdf"))
+                .contentType(MediaType.APPLICATION_PDF)
                 .body(resource);
     }
-
 }
